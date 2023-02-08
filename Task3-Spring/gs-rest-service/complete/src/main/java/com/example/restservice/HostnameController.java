ea7dd7594da68cc9951ff5184800924cd7fc6d62
Zak Foreman <zakky2k@yahoo.co.uk>
2023-02-08 14:47:53 +0300

    Updating Hostname and placing into Complete folder

---
+++ Task3-Spring/gs-rest-service/complete/src/main/java/com/example/restservice/HostnameController.java
@@ -0,0 +1,27 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HostnameController {
+
+	private static final String template = "Your hostname is: %s";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/hostname")
+	public Hostname hostname(@RequestParam(value = "Hostname", defaultValue = "localhost") String name) {
+		try {
+			InetAddress id = InetAddress.getLocalHost();
+			return new Hostname(counter.incrementAndGet(), String.format(template, id.getHostName()));
+	} catch (UnknownHostException e) { 
+		return new Hostname(counter.incrementAndGet(), String.format(template, "UnknownHost"));
+		}
+	}
+}
+
